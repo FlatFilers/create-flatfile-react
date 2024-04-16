97f7162aaec58f8fa98d6f75bbf0cf066c0668c6
Ashley Mulligan <ashleygmulligan@gmail.com>
2024-04-16 10:30:25 -0500

    feat: new properties to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,52 @@
-import { ISpace, initializeFlatfile } from "@flatfile/react";
-import { useState } from "react";
-import { listener } from "./listeners/simple";
+import { FlatfileProvider } from "@flatfile/react";
 import { workbook } from "./workbook";
+import { useFlatfile, Workbook } from "@flatfile/react";
 
-export default function App() {
-  const spaceProps: ISpace = {
-    name: "Embedded Space",
-    publishableKey: "YOUR_PUBPLISHABLE_KEY",
-    environmentId: "YOUR_ENVIRONMENT_ID",
-    workbook,
-    listener,
-    sidebarConfig: {
-      showSidebar: false,
-    },
-    themeConfig: {
-      root: {
-        primaryColor: "red",
-      },
-      sidebar: {
-        logo: "https://images.ctfassets.net/hjneo4qi4goj/gL6Blz3kTPdZXWknuIDVx/7bb7c73d93b111ed542d2ed426b42fd5/flatfile.svg",
-      },
-    },
-    closeSpace: {
-      operation: "submitActionFg",
-      onClose: () => setShowSpace(false),
-    },
-  };
+export default function Home() {
+  const PUBLISHABLE_KEY = "pk_d9GtgYgMAlrylXOXp8ps93fNxgPe4zuD";
 
-  const [showSpace, setShowSpace] = useState(false);
-  const { Space, OpenEmbed } = initializeFlatfile(spaceProps);
+  const App = () => {
+    const { open, openPortal, closePortal } = useFlatfile();
 
-  const onOpenSpace = async () => {
-    setShowSpace(!showSpace);
-    await OpenEmbed();
+    return (
+      <div className="content">
+        <h2>
+          <code>&lt;Flatfile /&gt;</code>
+        </h2>
+        <p>Embed Flatfile in just a few lines of code.</p>
+        {/*Button to trigger the modal */}
+        <div>
+          <button
+            className="contrast"
+            onClick={() => {
+              open ? closePortal() : openPortal();
+            }}
+          >
+            {open === true ? "Close" : "Open and create new"} Space
+          </button>
+        </div>
+
+        <Workbook
+          config={workbook}
+          onSubmit={async (sheet) => {
+            console.log("on Workbook Submit ", { sheet });
+          }}
+          onRecordHooks={[
+            [
+              (record) => {
+                record.set("lastName", "Rock");
+                return record;
+              },
+            ],
+          ]}
+        ></Workbook>
+      </div>
+    );
   };
 
   return (
-    <div className="content">
-      <h2>
-        <code>&lt;Flatfile /&gt;</code>
-      </h2>
-      <p>Embed Flatfile in just a few lines of code.</p>
-      {/*Button to trigger the modal */}
-      <div>
-        <button className="contrast" onClick={onOpenSpace}>
-          {showSpace === true ? "Close" : "Open and create new"} Space
-        </button>
-        {showSpace && <Space />}
-      </div>
-    </div>
+    <FlatfileProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </FlatfileProvider>
   );
 }
